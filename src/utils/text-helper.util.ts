e88f431b1548d02c330630b453e12f4a08516b6f
khanh-viet-le <khanh.viet.le@agest.vn>
2026-01-16 14:40:26 +0700

    refactor: util structure

--- src/utils/text-helper.util.ts
+++ src/utils/text-helper.util.ts
@@ -1,5 +1,7 @@
-export function extractNumbers(text: string): number[] {
-  const matches = text.match(/\d{1,3}(?:,\d{3})*(?:\.\d+)?|\d+(?:\.\d+)?/g);
+export class TextHelper {
+  static extractNumbers(text: string): number[] {
+    const matches = text.match(/\d{1,3}(?:,\d{3})*(?:\.\d+)?|\d+(?:\.\d+)?/g);
 
-  return matches ? matches.map((n) => Number(n.replace(/,/g, ""))) : [];
+    return matches ? matches.map((n) => Number(n.replace(/,/g, ""))) : [];
+  }
 }
