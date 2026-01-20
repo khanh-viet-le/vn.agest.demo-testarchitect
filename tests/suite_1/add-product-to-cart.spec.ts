da1a48c9caf643ea35d3809b823ecdf620e36a80
khanh-viet-le <khanh.viet.le@agest.vn>
2026-01-20 13:12:54 +0700

    chore: add tag, annotation for demo ui mode

--- tests/suite_1/add-product-to-cart.spec.ts
+++ tests/suite_1/add-product-to-cart.spec.ts
@@ -4,43 +4,50 @@ import { expect } from "@playwright/test";
 import { ShopPage } from "@pages/shop.page";
 import { ProductPage } from "@pages/product.page";
 
-test("TC_04: Verify Product Can Be Added to Shopping Cart", async ({
-  homePage,
-  page,
-}) => {
-  // 1. Navigate to Shop page
-  await homePage.navigateToPageInMainMenu("Shop");
-  const shopPage = new ShopPage(page);
+test(
+  "TC_04: Verify Product Can Be Added to Shopping Cart",
+  {
+    tag: ["@cart", "@product"],
+    annotation: {
+      type: "test",
+      description: "Here is an annotation test",
+    },
+  },
+  async ({ homePage, page }) => {
+    // 1. Navigate to Shop page
+    await homePage.navigateToPageInMainMenu("Shop");
+    const shopPage = new ShopPage(page);
 
-  // 2. Select any available product
-  await shopPage.selectFirstAvailableProduct();
-  const productPage = new ProductPage(page);
-  const oldCartCount = await productPage.getCartCount();
-  const shownProduct = await productPage.getProduct();
+    // 2. Select any available product
+    await shopPage.selectFirstAvailableProduct();
+    const productPage = new ProductPage(page);
+    const oldCartCount = await productPage.getCartCount();
+    const shownProduct = await productPage.getProduct();
 
-  // 3. Click "Add to Cart" button
-  await productPage.addToCart();
+    // 3. Click "Add to Cart" button
+    await productPage.addToCart();
 
-  // 4. Verify cart notification
-  // 5. Check cart icon update
-  // - Product should be added to cart
-  const productsInCart = await productPage.getProductsInCart();
-  expect
-    .soft(
-      productsInCart.find((product) => product.title === shownProduct.title)
-    )
-    .toBeTruthy();
+    // 4. Verify cart notification
+    // 5. Check cart icon update
+    // - Product should be added to cart
+    const productsInCart = await productPage.getProductsInCart();
+    expect
+      .soft(
+        productsInCart.find((product) => product.title === shownProduct.title),
+      )
+      .toBeTruthy();
 
-  // - Cart count should increase
-  const newCartCount = await productPage.getCartCount();
-  expect.soft(newCartCount).toBe(oldCartCount + 1);
+    // - Cart count should increase
+    const newCartCount = await productPage.getCartCount();
+    expect.soft(newCartCount).toBe(oldCartCount + 1);
 
-  // - Cart total should update
-  const newCartTotalPrice = await productPage.getCartTotalPrice();
-  expect.soft(newCartTotalPrice).toBe(shownProduct.price);
+    // - Cart total should update
+    const newCartTotalPrice = await productPage.getCartTotalPrice();
+    expect.soft(newCartTotalPrice).toBe(shownProduct.price);
 
-  // - Success message should appear
-  expect
-    .soft(await productPage.getMessageStatus())
-    .toBe(MessageStatusConstants.SUCCESS);
-});
+    // - Success message should appear
+    expect
+      .soft(await productPage.getMessageStatus())
+      .toBe(MessageStatusConstants.SUCCESS);
+  },
+);
--- tests/suite_1/update-product-in-cart.spec.ts
+++ tests/suite_1/update-product-in-cart.spec.ts
@@ -5,32 +5,35 @@ import { expect } from "@playwright/test";
 // Product is in cart
 const test = addedToCartTest;
 
-test("TC_05: Verify Product Quantity Can Be Updated in Cart", async ({
-  productPage,
-  page,
-}) => {
-  // 1. Navigate to Cart page
-  await productPage.navigateToCartPage();
-  const cartPage = new CartPage(page);
+test(
+  "TC_05: Verify Product Quantity Can Be Updated in Cart",
+  {
+    tag: "@cart",
+  },
+  async ({ productPage, page }) => {
+    // 1. Navigate to Cart page
+    await productPage.navigateToCartPage();
+    const cartPage = new CartPage(page);
 
-  const oldCartTotal = await cartPage.getCartTotal();
-  const updatedQuantity = 2;
+    const oldCartTotal = await cartPage.getCartTotal();
+    const updatedQuantity = 2;
 
-  // 2. Locate quantity field
-  // 3. Change quantity to 2
-  // 4. Click "Update Cart" button
-  await cartPage.updateFirstAvailableProductQuantity(updatedQuantity);
+    // 2. Locate quantity field
+    // 3. Change quantity to 2
+    // 4. Click "Update Cart" button
+    await cartPage.updateFirstAvailableProductQuantity(updatedQuantity);
 
-  // 5. Verify cart updates
-  // - update message should appear
-  expect(cartPage.messageLocator).toHaveText(/Cart updated/);
+    // 5. Verify cart updates
+    // - update message should appear
+    expect(cartPage.messageLocator).toHaveText(/Cart updated/);
 
-  const newQuantity = await cartPage.getFirstAvailableProductQuantity();
-  const newCartTotal = await cartPage.getCartTotal();
+    const newQuantity = await cartPage.getFirstAvailableProductQuantity();
+    const newCartTotal = await cartPage.getCartTotal();
 
-  // - Quantity should update
-  expect.soft(newQuantity).toBe(updatedQuantity);
+    // - Quantity should update
+    expect.soft(newQuantity).toBe(updatedQuantity);
 
-  // - Cart total should recalculate
-  expect.soft(newCartTotal).toBe(oldCartTotal * updatedQuantity);
-});
+    // - Cart total should recalculate
+    expect.soft(newCartTotal).toBe(oldCartTotal * updatedQuantity);
+  }
+);
